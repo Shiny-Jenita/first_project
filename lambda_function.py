1ba679ef7c0894155b012749e4b7014b3921f7c4
Shiny-Jenita <shinyjenita@entrans.io>
2025-09-15 14:14:02 +0530

    automation of  deploy&testing

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,12 @@
 import json
-
-def lambda_handler(event, context):
+def lambda_handler(event,context):
     # Example: log the event
     print("Received event:", json.dumps(event))
 
     # Business logic goes here
     result = {
         "statusCode": 200,
-        "body": json.dumps({"message": "Lambda Triggered again......."})
+        "body": json.dumps({"message": "Lambda Triggered ..."})
     }
 
     return result
